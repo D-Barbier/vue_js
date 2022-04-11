a8c419d97c54f70be7cf14a9f2f7c9b79ab5745f
DBarbier <b.damien@live.fr>
2022-04-11 11:09:32 +0200

    maj intro

--- 01-intro/main.js
+++ 01-intro/main.js
@@ -43,6 +43,21 @@ const app ={
     methods:{
         reinitialiser(){
             this.nombre=0;
+        },
+
+        supprimerFilm(event){
+            let id = event.target.dataset.id;
+           
+            // let result = [];
+            // for(let movie of movies ){
+            //     if (movie.movie_id !=id){
+            //         result.push(movie);
+            //     }
+            // }
+
+            this.movies = this.movies.filter(movie => movie.movie_id != id);
+
+            console.log(event.target.dataset.id);
         }
     }//fin de methods
 
